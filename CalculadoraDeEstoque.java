7e0bd7a389c5f5770b659f0eb6a07b08ea6524cd
Isabelle Silva <bellinha292@gmail.com>
2026-09-09 15:59:14 -0300

    Create CalculadoraDeEstoque.java

---
+++ CalculadoraDeEstoque.java
@@ -0,0 +1,11 @@
+public class CalculadoraDeEstoque {
+    public static void main(String[] args) {
+        double livroJava8 = 40.50; //Variável para armazenar um valor + 'double'=ponto flutuante (float)
+        double livroTDD = 30.00;
+
+        double soma = livroJava8 + livroTDD;
+
+        System.out.println("O total em estoque é "+ soma);
+    }
+    
+}
